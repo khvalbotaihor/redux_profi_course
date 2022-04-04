c278174b73553853852828303cd250b037547c66
khvalbotaihor <ihorkhvalbota@gmail.com>
2022-04-04 20:00:27 +0300

    - reduxt toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import {userSlice} from "./store/reducers/UserSlice";
+import {useAppDispatch, useAppSelector} from "./hooks/redux";
 
 function App() {
+  const {count} = useAppSelector(state => state.userReducer)
+  const {increment} = userSlice.actions;
+  const dispatch = useAppDispatch()
+
   return (
     <div className="App">
-
+        <h1>{count}</h1>
+        <button onClick={() => dispatch(increment(10))}>Increment</button>
     </div>
   );
 }
--- src/store/reducers/UserSlice.ts
+++ src/store/reducers/UserSlice.ts
@@ -1,23 +1,27 @@
 import {IUser} from "../../models/IUser";
-import {createSlice} from "@reduxjs/toolkit";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 
 interface UserState {
     users: IUser[];
     isLoading: boolean;
     error: string;
+    count: number;
 }
 
 const initialState = {
     users: [],
     isLoading: false,
-    error: ''
+    error: '',
+    count: 0
 }
 
 export const userSlice = createSlice({
     name: 'user',
     initialState,
     reducers: {
-
+        increment(state, action: PayloadAction<number>){
+            state.count += action.payload;
+        }
     }
 })
 
