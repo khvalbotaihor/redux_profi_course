ee3bb07e08646f755bcbc707b90d6fcc0b00b3d0
khvalbotaihor <ihorkhvalbota@gmail.com>
2022-04-05 10:16:04 +0300

    - toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import {userSlice} from "./store/reducers/UserSlice";
 import {useAppDispatch, useAppSelector} from "./hooks/redux";
+import {fetchUsers} from "./store/reducers/ActionCreators";
 
 function App() {
   const dispatch = useAppDispatch()
+  const {users, isLoading, error} = useAppSelector(state => state.userReducer)
+  useEffect(() => {
+    dispatch(fetchUsers())
+  },[])
 
   return (
     <div className="App">
-
+      {isLoading && <h1>Loading .....</h1>}
+      {error && <h1>{error}</h1>}
+      {JSON.stringify(users, null, 2)}
     </div>
   );
 }
--- src/store/reducers/ActionCreators.ts
+++ src/store/reducers/ActionCreators.ts
@@ -1,11 +1,22 @@
 import {AppDispatch} from "../store";
 import axios from "axios";
 import {IUser} from "../../models/IUser";
+import {userSlice} from "./UserSlice";
+import {createAsyncThunk} from "@reduxjs/toolkit";
 
 export const fetchUsers = () => async (dispatch: AppDispatch) => {
     try {
+        dispatch(userSlice.actions.usersFetching())
         const responce = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
-    }catch (e) {
+        dispatch(userSlice.actions.usersFetchingSuccess(responce.data))
 
+    }catch (e) {
+        // @ts-ignore
+        dispatch(userSlice.actions.usersFetchingError(e.message))
     }
 }
+
+export const fetchUsers = createAsyncThunk(
+    'user/fetchAll',
+    async ()
+)
